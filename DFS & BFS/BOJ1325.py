5c8c8d47caf67caf2b72b8aafe06670e9b8cafaa
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-03-19 13:42:52 +0900

    [BOJ] 효율적인 해킹 / 실버 1 / 성공

---
+++ DFS & BFS/BOJ1325.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+def bfs(v) :
+    deq = deque([v])
+    visited = [False] * (n + 1)
+    visited[v] = True
+    count = 1
+    while deq :
+        v = deq.popleft()
+        for e in arr[v] :
+            if not (visited[e]) :
+                deq.append(e)
+                visited[e] = True
+                count += 1
+
+    return count
+
+n, m = map(int, input().split(' ')) 
+arr = [[] for _ in range(n + 1)]
+
+for _ in range(m) :
+    y, x = map(int, input().split(' '))
+    arr[x].append(y)
+
+
+result = []
+max_value = -1
+
+for i in range(1, n + 1) :
+    c = bfs(i)
+    if c > max_value :
+        result = [i]
+        max_value = c        
+    elif c == max_value : 
+        result.append(i)
+        max_value = c
+
+for e in result :
+    print(e, end = ' ')
