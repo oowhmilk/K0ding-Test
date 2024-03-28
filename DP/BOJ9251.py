05975a2d7de96c82b12e723acf0e3a4d1dfe0631
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-03-28 19:58:39 +0900

    [BOJ] LCS / 골드 5 / 성공

--- DP/BOJ9251.py
+++ DP/BOJ9251.py
@@ -1,16 +1,12 @@
-import sys
-input = sys.stdin.readline
+x = input()
+y = input()
 
-a = input()
-b = input()
-
-dp = [[0] * (len(b) + 1) for _ in range(len(a) + 1)]
-
-for i in range(1, len(a) + 1) :
-    for j in range(1, len(b) + 1) :
-        if a[i] == b[j] :
+dp = [[0] * (len(y) + 1) for _ in range(len(x) + 1)]
+for i in range(1, len(x) + 1):
+    for j in range(1, len(y) + 1):
+        if x[i - 1] == y[j - 1]:
             dp[i][j] = dp[i - 1][j - 1] + 1
-        else : 
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        else:
+            dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
 
-print(dp[len(a)][len(b)])
+print(dp[len(x)][len(y)])
