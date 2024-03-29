2080d89ed56e2c2e783d746f2ca4235415ad9070
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-03-29 15:17:53 +0900

    [BOJ] 배 / 골드 5 / 성공

---
+++ 그리디/BOJ1092.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+n = int(input())
+crane = list(map(int, input().split(' ')))
+
+m = int(input())
+box = list(map(int, input().split(' ')))
+
+# 정렬
+crane.sort()
+box.sort() 
+
+# 내림차순 정렬
+crane.reverse()
+box.reverse()
+
+if max(crane) < max(box) :
+    print(-1)
+    sys.exit()
+
+count = 0      
+i = 0            
+while len(box) :
+    for j in range(len(box)) :
+        if crane[i] >= box[j] : 
+            box.remove(box[j])
+            if i != n - 1 and len(box) == 0 :
+                count += 1
+            break
+
+    i += 1
+
+    if i == n :
+        count += 1
+        i = 0 
+
+print(count)
