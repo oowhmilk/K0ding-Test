9b7cc457013e8acafca7039b0620bc93e2ef38fc
oowhmilk <rlagusdn3046@naver.com>
2024-07-11 16:20:06 +0900

    [BOJ] 가장높은탑쌓기 / 골드 3 / 성공

--- DP/BOJ2655.py
+++ DP/BOJ2655.py
@@ -33,3 +33,37 @@ result.reverse()
 print(len(result))
 [print(i) for i in result]
 
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+
+arr.append((0, 0, 0, 0))
+for i in range(1, n + 1) :
+    a, h, w = map(int, input().split(' ')) # a : 넓이, h : 높이, w : 무게
+    arr.append((i, a, h, w))
+
+arr.sort(key = lambda x : x[3]) # 무게 기준 sort
+
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1) :
+    for j in range(i) :
+        if arr[j][1] < arr[i][1] :
+            dp[i] = max(dp[i], arr[i][2] + dp[j])
+
+max_value = max(dp)
+index = n
+result = []
+
+while index != 0 :
+    if max_value == dp[index] :
+        result.append(arr[index][0])
+        max_value -= arr[index][2]
+    index -= 1
+result.reverse()
+
+print(len(result))
+[print(i) for i in result]
