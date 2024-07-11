438c34c899b0e2b7cf14329f6591e957503b04f7
oowhmilk <rlagusdn3046@naver.com>
2024-07-11 17:12:30 +0900

    [BOJ] 숨바꼭질 / 실버 1 / 성공

--- DFS & BFS/BOJ1697.py
+++ DFS & BFS/BOJ1697.py
@@ -1,24 +1,46 @@
+# import sys
+# input = sys.stdin.readline
+
+# from collections import deque
+
+# def bfs(n) :
+#     deq = deque([n])
+
+#     while deq :
+#         v = deq.popleft()
+
+#         if v == k :
+#             return array[v]
+#         for next in (v - 1, v + 1, v * 2) :
+#             if 0 <= next < 100001 and not array[next] :
+#                 array[next] = array[v] + 1
+#                 deq.append(next)
+
+
+# n, k = map(int, input().split(' '))
+
+# array = [0] * (2000000 + 1)
+
+# print(bfs(n))
+
 import sys
 input = sys.stdin.readline
 
 from collections import deque
 
 def bfs(n) :
-    deq = deque([n])
+    q = deque([n])
 
-    while deq :
-        v = deq.popleft()
-
+    while q :
+        v = q.popleft()
         if v == k :
-            return array[v]
-        for next in (v - 1, v + 1, v * 2) :
-            if 0 <= next < 100001 and not array[next] :
-                array[next] = array[v] + 1
-                deq.append(next)
+            return arr[v]
+        for next in v - 1, v + 1, v * 2 :
+            if 0 <= next < 100001 and not arr[next] :
+                arr[next] = arr[v] + 1
+                q.append(next)
 
 
 n, k = map(int, input().split(' '))
-
-array = [0] * (2000000 + 1)
-
+arr = [0] * (200001)
 print(bfs(n))
