0e7e7dc68827248b4b95f8fef3e0b400f121fef7
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-07-15 23:33:57 +0900

    [BOJ] 거스름돈 / 브론즈 2 / 성공

--- 그리디/BOJ5585.py
+++ 그리디/BOJ5585.py
@@ -10,3 +10,15 @@ for i in [500,100,50,10,5,1] :
     change %= i
 
 print(count)
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+change = 1000 - n
+
+for i in [500, 100, 50, 10, 5, 1] :
+    count += change // i
+    change = change % i
+
+print(count)
