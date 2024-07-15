5b1875e2b278a65373ffe5c87ec7565ba8ac246e
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-07-15 21:29:57 +0900

    [BOJ] 우주신과의 교감 / 골드 3 / 성공

--- DFS & BFS/BOJ1774.py
+++ DFS & BFS/BOJ1774.py
@@ -79,6 +79,29 @@ def get_distance(p1, p2) :
     y = p1[1] - p2[1]
     return math.sqrt((x * x) + (y * y))
 
+def get_parent(parent, n) :
+    if parent[n] == n :
+        return n
+    return get_parent(parent, parent[n])
+
+def union_parent(parent, a, b) :
+    a = get_parent(parent, a)
+    b = get_parent(parent, b)
+
+    if a < b :
+        parent[b] = a
+    else :
+        parent[a] = b
+
+def find_parent(parent, a, b) :
+    a = get_parent(parent, a)
+    b = get_parent(parent, b)
+
+    if a == b :
+        return True
+    else : 
+        return False
+
 edges = []
 parent = {}
 locations = []
@@ -100,3 +123,13 @@ for i in range(1, n + 1) :
 for i in range(m) :
     a, b = map(int, input().split(' '))
     union_parent(parent, a, b)
+
+edges.sort(key = lambda x : x[2])
+
+result = 0
+for a, b, cost in edges :
+    if not find_parent(parent, a, b) :
+        union_parent(parent, a, b)
+        result += cost
+
+print("0.2f" % result)
