68c74eb0aab7391ba3acffcc6977cbff132c5437
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-07-17 00:05:50 +0900

    [BOJ] 뒤집기 / 실버 5 / 성공

--- 그리디/BOJ1439.py
+++ 그리디/BOJ1439.py
@@ -1,3 +1,30 @@
+# 경우의 수는 단 2가지만 고려하면 된다.
+# 모든 수를 0 으로 만드는 경우, 모든 수를 1 로 만드는 경우 
+# 모든 수를 1로 만드는 경우 : 첫번째 원소가 0 인 경우 , 2개씩 원소를 비교할 때, 1 에서 0 으로 바뀌는 경우 => 각 경우 마다 +1 씩 증가
+# 모든 수를 0 으로 만드는 경우 : 첫번째 원소가 1 인 경우, 2개씩 원소를 비교할 때, 0 에서 1 로 바뀌는 경우 => 각 경우 마다 +1 씩 증가 
+
+import sys
+input = sys.stdin.readline
+
+data = input()
+
+count0 = 0 # 모든 수를 0 으로 만드는 경우
+count1 = 0 # 모든 수를 1 로 만드는 경우 
+
+if data[0] == 0 :
+    count1 += 1
+else :
+    count0 += 1
+
+for i in range(len(data) - 1) :
+    if data[i] != data[i + 1] :
+        if data[i + 1] == 1 :
+            count0 += 1
+        else :
+            count1 += 1
+
+print(min(count0, count1))
+
 import sys
 input = sys.stdin.readline
 
