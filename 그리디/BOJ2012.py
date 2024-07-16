975a516f614e8d64f0abbb6d43acabb9aca71a73
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-07-17 00:06:10 +0900

    [BOJ] 등수 매기기 / 실버 3 / 성공

--- 그리디/BOJ2012.py
+++ 그리디/BOJ2012.py
@@ -1,16 +1,34 @@
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+
+# arr = []
+# for _ in range(n) :
+#     arr.append(int(input()))
+
+# arr.sort()
+
+# dif = 0
+# for i in range(n) :
+#     dif += abs(arr[i] - (i + 1))
+
+# print(dif)
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-
 arr = []
+
 for _ in range(n) :
     arr.append(int(input()))
 
 arr.sort()
 
 dif = 0
+
 for i in range(n) :
-    dif += abs(arr[i] - (i + 1))
+    dif += abs((i + 1) - arr[i])
 
 print(dif)
