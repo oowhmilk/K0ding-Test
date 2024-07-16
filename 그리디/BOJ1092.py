03e8c56730e650fa13d2a7bbfcfc2b533132e74c
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-07-17 00:05:27 +0900

    [BOJ] 배 / 골드 5 / 성공

--- 그리디/BOJ1092.py
+++ 그리디/BOJ1092.py
@@ -1,40 +1,69 @@
+# import sys
+# input = sys.stdin.readline
+
+# from collections import deque
+
+# n = int(input())
+# crane = list(map(int, input().split(' ')))
+
+# m = int(input())
+# box = list(map(int, input().split(' ')))
+
+# # 정렬
+# crane.sort()
+# box.sort() 
+
+# # 내림차순 정렬
+# crane.reverse()
+# box.reverse()
+
+# if max(crane) < max(box) :
+#     print(-1)
+#     sys.exit()
+
+# count = 0      
+# i = 0            
+# while len(box) :
+#     for j in range(len(box)) :
+#         if crane[i] >= box[j] : 
+#             box.remove(box[j])
+#             if i != n - 1 and len(box) == 0 :
+#                 count += 1
+#             break
+
+#     i += 1
+
+#     if i == n :
+#         count += 1
+#         i = 0 
+
+# print(count)
+
+
 import sys
 input = sys.stdin.readline
 
-from collections import deque
-
 n = int(input())
-crane = list(map(int, input().split(' ')))
+crane = list(map(int, input().split()))
 
 m = int(input())
-box = list(map(int, input().split(' ')))
+box = list(map(int, input().split()))
 
-# 정렬
-crane.sort()
-box.sort() 
+crane.sort(reverse=True)
+box.sort(reverse=True)
 
-# 내림차순 정렬
-crane.reverse()
-box.reverse()
-
-if max(crane) < max(box) :
+if crane[0] < box[0]:
     print(-1)
     sys.exit()
 
-count = 0      
-i = 0            
+time = 0
 while len(box) :
-    for j in range(len(box)) :
-        if crane[i] >= box[j] : 
-            box.remove(box[j])
-            if i != n - 1 and len(box) == 0 :
-                count += 1
-            break
+    for i in range(n) :
+        for j in range(len(box)) :
+            if crane[i] >= box[j] :
+                box.remove(box[j])
+                break
 
-    i += 1
+    time += 1
 
-    if i == n :
-        count += 1
-        i = 0 
-
-print(count)
+print(time)
