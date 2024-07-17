95a0b4101f99853d198c5ff74432e10d6178c8ad
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-07-17 22:48:54 +0900

    [BOJ] 컵라면 / 골드 2 / 성공

--- 그리디/BOJ1781.py
+++ 그리디/BOJ1781.py
@@ -1,3 +1,31 @@
+# 데드라인을 초과하는 문제를 풀 수 없다. 
+# 정렬과 우선 순위 큐 => O(NlogN)
+
+# import sys
+# input = sys.stdin.readline
+# import heapq
+
+# n = int(input())
+
+# arr = []
+# for i in range(n) :
+#     deadline, cup = map(int, input().split(' '))
+#     arr.append((deadline, cup))
+
+# arr.sort()
+
+# heap = []
+# for i in range(n) :
+#     deadline, cup = arr[i]
+#     heapq.heappush(heap, cup)
+    
+#     if len(heap) > deadline :
+#         heapq.heappop(heap)
+
+# print(sum(heap))
+
+
+
 import sys
 input = sys.stdin.readline
 import heapq
@@ -5,18 +33,25 @@ import heapq
 n = int(input())
 
 arr = []
-for i in range(n) :
-    deadline, cup = map(int, input().split(' '))
-    arr.append((deadline, cup))
+for _ in range(n) :
+    deadline, noodle = map(int, input().split(' '))
+    arr.append((deadline, noodle))
 
 arr.sort()
 
 heap = []
+time = 0
 for i in range(n) :
-    deadline, cup = arr[i]
-    heapq.heappush(heap, cup)
-    
-    if len(heap) > deadline :
+    # deadline, noodle = arr[i]
+    # print(arr[i][1])
+    heapq.heappush(heap, arr[i][1])
+
+    if len(heap) > arr[i][0] :
         heapq.heappop(heap)
 
-print(sum(heap))
+print(heap)
+result = 0
+for i in range(len(heap)) :
+    result += heapq.heappop(heap)
+
+print(result)
