af90c8cf069679719aafe76827ea299102d77f05
oowhmilk <rlagusdn3046@naver.com>
2024-07-17 10:57:39 +0900

    [BOJ] 센서 / 골드 5 / 성공

--- 그리디/BOJ2212.py
+++ 그리디/BOJ2212.py
@@ -1,21 +1,49 @@
+# 각 센서를 오름차순 정렬
+# 정렬된 센서를 K 개의 영역으로 나누는 것과 같음
+# 영역을 나눌때 어떻게 나누냐? 
+# 각 센서 간의 거리를 구하고 거리가 큰 것부터 K - 1 개 삭제
+# 삭제 이후 남은 거리를 전부 더하면 끝
+
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# k = int(input())
+
+# arr = list(map(int, input().split(' ')))
+# arr.sort()
+
+# if k >= n :
+#     print(0)
+#     sys.exit()
+
+# dif = []
+# for i in range(len(arr) - 1) :
+#     dif.append(arr[i + 1] - arr[i])
+
+# dif.sort(reverse = True)
+
+# for i in range(k - 1) :
+#     dif[i] = 0
+
+# print(sum(dif))
+
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 k = int(input())
-
 arr = list(map(int, input().split(' ')))
+
 arr.sort()
 
-if k >= n :
-    print(0)
-    sys.exit()
-
 dif = []
+
 for i in range(len(arr) - 1) :
     dif.append(arr[i + 1] - arr[i])
 
-dif.sort(reverse = True)
+dif = sorted(dif, reverse=True)
 
 for i in range(k - 1) :
     dif[i] = 0
