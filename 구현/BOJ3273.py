f35b4e80b4e96ef6d0b5a86a92aeb96d61d73e41
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-07-20 17:44:16 +0900

    [BOJ] 두 수의 합 / 실버 3 / 성공

--- 구현/BOJ3273.py
+++ 구현/BOJ3273.py
@@ -1,3 +1,32 @@
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# arr = list(map(int, input().split(' ')))
+# x = int(input())
+
+# arr = sorted(arr)
+
+# low = 0 
+# high = len(arr) - 1
+
+# result = 0
+# for i in range(n) :
+#     if high <= low :
+#         break
+
+#     if arr[low] + arr[high] == x :
+#         result += 1
+#         low += 1
+#         high -= 1
+#     elif arr[low] + arr[high] > x :
+#         high -= 1
+#     elif arr[low] + arr[high] < x :
+#         low += 1
+
+# print(result)
+
+
 import sys
 input = sys.stdin.readline
 
@@ -5,23 +34,23 @@ n = int(input())
 arr = list(map(int, input().split(' ')))
 x = int(input())
 
-arr = sorted(arr)
+arr.sort()
 
-low = 0 
-high = len(arr) - 1
-
+first = 0 
+last = len(arr) - 1
 result = 0
-for i in range(n) :
-    if high <= low :
-        break
 
-    if arr[low] + arr[high] == x :
+while first < last :
+    left = arr[first]
+    right = arr[last]
+
+    if left + right == x :
+        first += 1
+        last -= 1
         result += 1
-        low += 1
-        high -= 1
-    elif arr[low] + arr[high] > x :
-        high -= 1
-    elif arr[low] + arr[high] < x :
-        low += 1
+    elif left + right < x :
+        first += 1
+    elif left + right > x :
+        last -= 1
 
 print(result)
