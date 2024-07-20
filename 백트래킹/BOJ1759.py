b5fb8708e2a26b6b3252670307f52fe50ff3ec43
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-07-20 17:47:43 +0900

    [BOJ] 암호 만들기 / 골드 5 / 성공

--- 백트래킹/BOJ1759.py
+++ 백트래킹/BOJ1759.py
@@ -1,25 +1,48 @@
+# import sys
+# input = sys.stdin.readline
+
+# from itertools import combinations
+# vowels = ('a', 'e', 'i', 'o', 'u')
+# l, c = map(int, input().split(' '))
+
+# input_str = input()
+# cleaned_str = input_str.strip()
+# array = list(cleaned_str.split(' '))
+# array.sort()
+
+# result = list(combinations(array, l))
+
+# # 길이가 l인 모든 암호 조합을 확인
+# for password in result:
+#     # 모음의 개수를 세기
+#     count = 0
+#     for i in password:
+#         if i in vowels:
+#             count += 1
+
+#     # 최소 한 개의 모음과 최소 두 개의 자음이 있는 경우 출력
+#     if count >= 1 and count <= l - 2:
+#         print(''.join(password))
+
+
 import sys
 input = sys.stdin.readline
 
 from itertools import combinations
-vowels = ('a', 'e', 'i', 'o', 'u')
+
 l, c = map(int, input().split(' '))
+arr = list(input().strip().split(' '))
+arr.sort()
 
-input_str = input()
-cleaned_str = input_str.strip()
-array = list(cleaned_str.split(' '))
-array.sort()
+vowels = ['a', 'e', 'i', 'o', 'u']
 
-result = list(combinations(array, l))
+result = list(combinations(arr, l))
 
-# 길이가 l인 모든 암호 조합을 확인
-for password in result:
-    # 모음의 개수를 세기
+for x in result :
     count = 0
-    for i in password:
-        if i in vowels:
+    for i in x :
+        if i in vowels :
             count += 1
 
-    # 최소 한 개의 모음과 최소 두 개의 자음이 있는 경우 출력
-    if count >= 1 and count <= l - 2:
-        print(''.join(password))
+    if count >= 1 and count <= l - 2 :
+        print(''.join(x))
