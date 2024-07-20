e0ceb24399a56e8e9eb919b5ab1dc2e9101251a9
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-07-20 17:44:45 +0900

    [BOJ] 통나무 건너뛰기 / 실버 1 / 성공

--- 구현/BOJ11497.py
+++ 구현/BOJ11497.py
@@ -1,33 +1,77 @@
+# import sys
+# input = sys.stdin.readline
+
+# ans = []
+
+# for _ in range(int(input())) :
+#     n = int(input())
+#     arr = list(map(int, input().split(' ')))
+#     arr = sorted(arr)
+
+#     result = [0] * (len(arr))
+#     left = 0
+#     right = len(arr) - 1
+
+#     for i in range(n) :
+#         if right < left :
+#             break
+
+#         if i % 2 == 0 :
+#             result[left] = arr[i]
+#             left += 1
+#         else : 
+#             result[right] = arr[i]
+#             right -= 1
+
+#     dif = 0
+#     for i in range(len(result) - 1) :
+#         dif = max(dif, abs(result[i] - result[i + 1]))
+
+#     ans.append(dif)
+
+# for data in ans :
+#     print(data)
+
+
 import sys
 input = sys.stdin.readline
 
-ans = []
+import heapq
 
 for _ in range(int(input())) :
     n = int(input())
     arr = list(map(int, input().split(' ')))
-    arr = sorted(arr)
 
-    result = [0] * (len(arr))
-    left = 0
-    right = len(arr) - 1
+    arr.sort(reverse=True)
 
-    for i in range(n) :
-        if right < left :
-            break
+    result = [0] * n
+    result[n // 2] = arr[0]
 
+    for i in range(1, n) :
         if i % 2 == 0 :
-            result[left] = arr[i]
-            left += 1
-        else : 
-            result[right] = arr[i]
-            right -= 1
+            result[n // 2 - i // 2 - 1] = arr[i]
+        else :
+            result[n // 2 + i // 2] = arr[i]
 
-    dif = 0
-    for i in range(len(result) - 1) :
-        dif = max(dif, abs(result[i] - result[i + 1]))
+    max_dif = 0
+    for i in range(n) :
+        dif = abs(result[i] - result[(i + 1) % n])
+        max_dif = max(max_dif, dif)
 
-    ans.append(dif)
+    print(max_dif)
 
-for data in ans :
-    print(data)
+
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())) :
+    n = int(input())
+    arr = list(map(int, input().split(' ')))
+    arr.sort()
+
+    max_dif = 0
+    for i in range(n - 2) :
+        dif = abs(arr[i] - arr[i + 2])
+        max_dif = max(max_dif, dif)
+
+    print(max_dif)
