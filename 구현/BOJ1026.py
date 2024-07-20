8927f116e1da22579fc72ff2daf569daefd9bfea
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-07-20 17:41:02 +0900

    [BOJ] 보물 / 실버 4 / 성공

--- 구현/BOJ1026.py
+++ 구현/BOJ1026.py
@@ -1,15 +1,32 @@
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# first = list(map(int, input().split(' ')))
+# second = list(map(int, input().split(' ')))
+
+# first = sorted(first)
+# second = sorted(second)
+
+# result = 0
+# for i in range(n) :
+#     result += (first[i] * second[n - 1 - i])
+
+# print(result)
+
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-first = list(map(int, input().split(' ')))
-second = list(map(int, input().split(' ')))
+fir = list(map(int, input().split(' ')))
+sec = list(map(int, input().split(' ')))
 
-first = sorted(first)
-second = sorted(second)
+fir.sort()
+sec.sort(reverse=True)
 
 result = 0
 for i in range(n) :
-    result += (first[i] * second[n - 1 - i])
+    result += fir[i] * sec[i]
 
 print(result)
