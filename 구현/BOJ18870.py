bc4103a3b83e6c0354fcf46fc3861be1ee7594e9
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-07-20 17:45:12 +0900

    [BOJ] 좌표 압축 / 실버 2 / 성공

--- 구현/BOJ18870.py
+++ 구현/BOJ18870.py
@@ -1,14 +1,34 @@
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# arr = list(map(int, input().split(' ')))
+# sorted_arr = set(arr)
+# sorted_arr = sorted(sorted_arr)
+
+# data = {}
+# for i in range(len(sorted_arr)) :
+#     data[sorted_arr[i]] = i
+
+# for i in range(len(arr)) :
+#     print(data[arr[i]], end = ' ')
+
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 arr = list(map(int, input().split(' ')))
-sorted_arr = set(arr)
-sorted_arr = sorted(sorted_arr)
+
+press = set()
+for x in arr :
+    press.add(x)
+
+press = sorted(press)
 
 data = {}
-for i in range(len(sorted_arr)) :
-    data[sorted_arr[i]] = i
+for i in range(len(press)) :
+    data[press[i]] = i
 
 for i in range(len(arr)) :
     print(data[arr[i]], end = ' ')
