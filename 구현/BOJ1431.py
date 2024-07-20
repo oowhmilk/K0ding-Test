df37294458b80a7b2530b37135c001c1958a90fd
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-07-20 17:41:21 +0900

    [BOJ] 시리얼 번호 / 실버 3 / 성공

--- 구현/BOJ1431.py
+++ 구현/BOJ1431.py
@@ -1,26 +1,49 @@
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+
+# result = []
+# for i in range(n) :
+#     data = input().strip()
+
+#     arr = []
+#     arr = list(data)
+
+#     length = len(arr)
+
+#     sum = 0
+#     for j in range(len(arr)) :
+#         if '0' <= arr[j] <= '9' :
+#             sum += int(arr[j])
+
+#     result.append((length, sum, data))
+
+# # result = sorted(result, key = lambda x : x[1])
+# result = sorted(result)
+
+# for i in result :
+#     print(i[2])
+
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
+arr = []
 
-result = []
-for i in range(n) :
-    data = input().strip()
-
-    arr = []
-    arr = list(data)
-
-    length = len(arr)
+for _ in range(n) :
+    str = input().strip()
+    length = len(str)
 
     sum = 0
-    for j in range(len(arr)) :
-        if '0' <= arr[j] <= '9' :
-            sum += int(arr[j])
+    for x in str : 
+        if '0' <= x and x <= '9' :
+            sum += int(x) 
 
-    result.append((length, sum, data))
+    arr.append((length, sum, str))
 
-# result = sorted(result, key = lambda x : x[1])
-result = sorted(result)
+arr = sorted(arr, key = lambda x : (x[0], x[1], x[2]))
 
-for i in result :
-    print(i[2])
+for x in arr :
+    print(x[2])
