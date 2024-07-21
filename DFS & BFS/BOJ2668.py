dc776a3320f3d449f569d62ea5f1ddac6a17a35c
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-07-21 20:00:35 +0900

    [BOJ] 숫자고르기 / 골드 5 / 성공

--- DFS & BFS/BOJ2668.py
+++ DFS & BFS/BOJ2668.py
@@ -1,40 +1,77 @@
+# import sys
+# input = sys.stdin.readline
+
+# sys.setrecursionlimit(1000000)
+
+# def is_cycle(x) :
+#     visited[x] = True
+
+#     y = graph[x]
+
+#     if not visited[y] :
+#         is_cycle(y)
+#     elif not finished[y] :
+#         while y != x :
+#             result.append(y)
+#             y = graph[y]
+#         result.append(x)
+
+#     finished[x] = True
+
+
+# n = int(input()) 
+# graph = [0] * (n + 1)
+
+# for i in range(1, n + 1) :
+#     graph[i] = int(input())
+
+# visited = [False] * (n + 1)
+# finished = [False] * (n + 1)
+# result = []
+
+# for x in range(1, n + 1) :
+#     if not visited[x] :
+#         is_cycle(x)
+
+# print(len(result))
+
+# result = sorted(result)
+# for x in result :
+#     print(x)
+
+
 import sys
 input = sys.stdin.readline
 
-sys.setrecursionlimit(1000000)
+sys.setrecursionlimit(100000)
 
 def is_cycle(x) :
     visited[x] = True
-
-    y = graph[x]
+    y = arr[x]
 
     if not visited[y] :
-        is_cycle(y)
+        is_cycle(y) 
     elif not finished[y] :
         while y != x :
             result.append(y)
-            y = graph[y]
+            y = arr[y]
         result.append(x)
-
     finished[x] = True
 
+n = int(input())
+arr = [0] * (n + 1)
 
-n = int(input()) 
-graph = [0] * (n + 1)
-
-for i in range(1, n + 1) :
-    graph[i] = int(input())
+for i in range(n) :
+    arr[i + 1] = int(input())
 
 visited = [False] * (n + 1)
 finished = [False] * (n + 1)
 result = []
 
-for x in range(1, n + 1) :
-    if not visited[x] :
-        is_cycle(x)
+for i in range(1, n + 1) :
+    is_cycle(arr[i])
 
+result.sort()
 print(len(result))
-
-result = sorted(result)
 for x in result :
     print(x)
