e15c7b026fae83e78cd758d646a537b7317c8ff8
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-07-25 00:01:37 +0900

    [BOJ] 최단경로 / 골드 4 / 성공

---
+++ 그래프/BOJ1753.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+
+import heapq 
+INF = int(1e9) 
+
+def dijkstra():
+    q = []
+    heapq.heappush(q, (0, start)) 
+    distance[start] = 0
+
+    while q: 
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost 
+                heapq.heappush(q, (cost, i[0]))
+
+n, m = map(int, input().split())
+start = int(input())
+
+graph = [[] for i in range(n + 1)]
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+dijkstra()
+
+for i in range (1, n + 1):
+    if distance[i] == 1e9:
+        print("INF")
+    else:
+        print(distance[i])
+        
