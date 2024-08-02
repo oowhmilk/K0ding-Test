f99e7245353d7e7e214d5c8016a7654406b6bf56
oowhmilk <rlagusdn3046@naver.com>
2024-08-02 10:14:43 +0900

    [BOJ] N-Queen / 골드 4 / 성공

--- 백트래킹/BOJ9663.py
+++ 백트래킹/BOJ9663.py
@@ -1,26 +1,56 @@
-# x번째 행에 놓은 Queen에 대해서 검증
-def check(x):
-    # 이전 행에서 놓았던 모든 Queen들을 확인
-    for i in range(x):
-        # 위쪽 혹은 대각선을 확인
-        if row[x] == row[i]:
+# # x번째 행에 놓은 Queen에 대해서 검증
+# def check(x):
+#     # 이전 행에서 놓았던 모든 Queen들을 확인
+#     for i in range(x):
+#         # 위쪽 확인
+#         if row[x] == row[i]:
+#             return False
+#         # 대각선 확인
+#         if abs(row[x] - row[i]) == x - i:
+#             return False
+#     return True
+
+# # x번째 행에 대하여 처리
+# def dfs(x):
+#     global result
+#     if x == n:
+#         result += 1
+#     else:
+#         # x번째 행의 각 열에 Queen을 둔다고 가정
+#         for i in range(n):
+#             row[x] = i
+#             # 해당 위치에 Queen을 두어도 괜찮은 경우
+#             if check(x):
+#                 # 다음 행으로 넘어가기
+#                 dfs(x + 1)
+
+# n = int(input())
+# row = [0] * n
+# result = 0
+# dfs(0)
+# print(result)
+
+
+import sys
+input = sys.stdin.readline
+
+def check(x) :
+    for i in range(x) :
+        if row[x] == row[i] :
             return False
-        if abs(row[x] - row[i]) == x - i:
+        if abs(row[x] - row[i]) == x - i :
             return False
     return True
 
-# x번째 행에 대하여 처리
-def dfs(x):
+def dfs(x) :
     global result
-    if x == n:
+    if x == n :
         result += 1
-    else:
-        # x번째 행의 각 열에 Queen을 둔다고 가정
-        for i in range(n):
+    else :
+        for i in range(n) :
             row[x] = i
-            # 해당 위치에 Queen을 두어도 괜찮은 경우
-            if check(x):
-                # 다음 행으로 넘어가기
+
+            if check(x) :
                 dfs(x + 1)
 
 n = int(input())
