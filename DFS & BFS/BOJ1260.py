20a95e0d9d658ba673218e319cca840f1d905a47
oowhmilk <rlagusdn3046@naver.com>
2024-08-12 11:25:15 +0900

    [BOJ] DFS와 BFS / 실버 2 / 성공

--- DFS & BFS/BOJ1260.py
+++ DFS & BFS/BOJ1260.py
@@ -36,36 +36,81 @@
 # bfs(v)
 
 
+# import sys
+# from collections import deque
+
+# def dfs(v) :
+#     print(v, end = ' ')
+#     visited[v] = True
+#     for e in adj[v] :
+#         if not (visited[e]) :
+#             dfs(e)
+
+# def bfs(v) :
+#     q = deque([v])
+#     while q :
+#         v = q.popleft()
+#         if not (visited[v]) :
+#             visited[v] = True
+#             print(v, end = ' ')
+#             for e in adj[v] :
+#                 if not visited[e] :
+#                     q.append(e)
+
+# n, m, v = map(int, input().split(' '))
+# adj = [[] for _ in range(n + 1)]
+
+# for _ in range(m) :
+#     x, y = map(int, input().split(' '))
+#     adj[x].append(y)
+#     adj[y].append(x)
+
+# for e in adj :
+#     e.sort()
+
+# visited = [False] * (n + 1)
+# dfs(v)
+# print()
+# visited = [False] * (n + 1)
+# bfs(v)
+
+
 import sys
+input = sys.stdin.readline
+
 from collections import deque
 
 def dfs(v) :
     print(v, end = ' ')
     visited[v] = True
-    for e in adj[v] :
-        if not (visited[e]) :
+    for e in arr[v] :
+        if not visited[e] :
             dfs(e)
 
 def bfs(v) :
-    q = deque([v])
+    q = deque()
+    q.append(v) 
+
     while q :
         v = q.popleft()
         if not (visited[v]) :
             visited[v] = True
             print(v, end = ' ')
-            for e in adj[v] :
+
+            for e in arr[v] :
                 if not visited[e] :
                     q.append(e)
 
+
 n, m, v = map(int, input().split(' '))
-adj = [[] for _ in range(n + 1)]
+arr = [[] for _ in range(n + 1)]
 
 for _ in range(m) :
     x, y = map(int, input().split(' '))
-    adj[x].append(y)
-    adj[y].append(x)
+    arr[x].append(y)
+    arr[y].append(x)
 
-for e in adj :
+for e in arr :
     e.sort()
 
 visited = [False] * (n + 1)
