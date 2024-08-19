9b3a72005c0ff179c39f39a42a35685e6400b895
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-08-19 23:21:26 +0900

    [BOJ] 순열 사이클 / 실버 3 / 성공

--- DFS & BFS/BOJ10451.py
+++ DFS & BFS/BOJ10451.py
@@ -1,47 +1,86 @@
+# import sys
+# input = sys.stdin.readline
+
+# def get_parent(parent, x) :
+#     if parent[x] == x :
+#         return x 
+#     return get_parent(parent, parent[x])
+
+# def union_parent(parent, x, y) :
+#     a = get_parent(parent, x)
+#     b = get_parent(parent, y)
+
+#     if a < b :
+#         parent[b] = a
+#     else :
+#         parent[a] = b
+
+# def find_parent(parent, x, y) :
+#     a = get_parent(parent, x)
+#     b = get_parent(parent, y)
+
+#     if a == b :
+#         return True
+#     else :
+#         return False
+
+# for _ in range(int(input())) :
+#     n = int(input())
+#     arr = []
+
+#     str = list(map(int, input().split(' ')))
+#     for i in range(len(str)) :
+#         arr.append((i + 1, str[i]))
+
+#     cycle = set()
+#     parent = {}
+#     for i in range(1, n + 1) :
+#         parent[i] = i
+     
+#     for a, b in arr :
+#         if not find_parent(parent, a, b) :
+#             union_parent(parent, a, b)
+
+#     for i in range(1, n + 1) :
+#         cycle.add(get_parent(parent, i))
+
+#     print(len(cycle))
+
+
 import sys
 input = sys.stdin.readline
 
-def get_parent(parent, x) :
-    if parent[x] == x :
-        return x 
-    return get_parent(parent, parent[x])
+def is_cycle(x) :
+    visited[x] = True
 
-def union_parent(parent, x, y) :
-    a = get_parent(parent, x)
-    b = get_parent(parent, y)
+    y = arr[x]
 
-    if a < b :
-        parent[b] = a
-    else :
-        parent[a] = b
+    if not visited[y] :
+        is_cycle(y)
+    elif not finished[y] :
+        while y != x :
+            result.append(y)
+            y = arr[y]
+        result.append(x)
 
-def find_parent(parent, x, y) :
-    a = get_parent(parent, x)
-    b = get_parent(parent, y)
+    finished[x] = True
 
-    if a == b :
-        return True
-    else :
-        return False
 
-for _ in range(int(input())) :
+for _ in range(int(input())) : 
     n = int(input())
+
     arr = []
-
     str = list(map(int, input().split(' ')))
-    for i in range(len(str)) :
-        arr.append((i + 1, str[i]))
+    for x in str :
+        arr.append(x - 1)
 
-    cycle = set()
-    parent = {}
-    for i in range(1, n + 1) :
-        parent[i] = i
-     
-    for a, b in arr :
-        if not find_parent(parent, a, b) :
-            union_parent(parent, a, b)
+    count = 0
+    visited = [False] * n
+    finished = [False] * n
+    result = []
+    for i in range(n) :
+        if not visited[i] :
+            is_cycle(i)
+            count += 1
 
-    for i in range(1, n + 1) :
-        cycle.add(get_parent(parent, i))
-
-    print(len(cycle))
+    print(count)
