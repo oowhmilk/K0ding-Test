bbe61e89571fbed7523ffc67a7fa6e9c519c3c2a
oowhmilk <rlagusdn3046@naver.com>
2024-08-20 11:03:49 +0900

    [BOJ] N-Queen / 골드 4 / 성공

--- 백트래킹/BOJ9663.py
+++ 백트래킹/BOJ9663.py
@@ -31,19 +31,51 @@
 # print(result)
 
 
+# import sys
+# input = sys.stdin.readline
+
+# def check(x) :
+#     for i in range(x) :
+#         if row[x] == row[i] :
+#             return False
+#         if abs(row[x] - row[i]) == x - i :
+#             return False
+#     return True
+
+# def dfs(x) :
+#     global result
+#     if x == n :
+#         result += 1
+#     else :
+#         for i in range(n) :
+#             row[x] = i
+
+#             if check(x) :
+#                 dfs(x + 1)
+
+# n = int(input())
+# row = [0] * n
+# result = 0
+# dfs(0)
+# print(result)
+
+
 import sys
 input = sys.stdin.readline
 
 def check(x) :
+
     for i in range(x) :
         if row[x] == row[i] :
             return False
-        if abs(row[x] - row[i]) == x - i :
+        elif abs(row[x] - row[i]) == x - i :
             return False
+        
     return True
 
 def dfs(x) :
     global result
+
     if x == n :
         result += 1
     else :
@@ -52,6 +84,7 @@ def dfs(x) :
 
             if check(x) :
                 dfs(x + 1)
+
 
 n = int(input())
 row = [0] * n
