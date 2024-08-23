5245e4a9197381ed4ed1ab8f61117c96f93b7141
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-08-23 22:26:46 +0900

    base.py update

--- base.py
+++ base.py
@@ -1,3 +1,5 @@
+# 1초에 5000만개 계산 가능
+
 ## 배열 초기화 
 # 4 * 5 크기 배열 
 arr = [[i] * 5 for i in range(4)]
@@ -18,6 +20,20 @@ for i in data:
         counter[i] += 1
 
 print(counter)
+
+## Dictionary 자료형 반복문 작성 법
+for key in counter :
+    value = counter[key]
+
+# Dictionary 자료형 value 값으로 정렬하는 법
+max(dict.values())
+
+# Dictionary 자료형 key 로 정렬
+sorted_dict = sorted(dict.itmes())
+
+
+# Dictionary 자료형 value 로 정렬
+sorted_dict = sorted(dict.itmes(), key = lambda x : x[1])
 
 ## Set 자료형 
 # 데이터의 중복을 허용하지 않고, 순서 상과 없을때 사용하는 형식
@@ -84,20 +100,23 @@ print(find_value(arr, 9))
 print(find_value(arr, 8))
 
 
-## 순열
+## 순열 (1, 2), (1, 3), (2, 1)
 from itertools import permutations
 arr = ['a', 'b', 'c']
 result = list(permutations(arr, 2))
 print(result)
 
-
-## 중복 순열
+## 중복 순열 (1, 1), (1, 2), (1, 3), (2, 1), (2, 2)
 from itertools import product
 result = list(product(arr, repeat=2))
 print(result)
 
+## 조합 (1, 2), (1, 3), (2, 3)
+from itertools import combinations
+result = list(combinations(arr, 2))
+print(result)
 
-## 중복 조합
+## 중복 조합 (1, 1), (1, 2), (1, 3), (2, 2), (2, 3)
 from itertools import combinations_with_replacement
 result = list(combinations_with_replacement(arr, 2))
 print(result)
@@ -143,7 +162,65 @@ print(arr)
 import sys
 input  = sys.stdin.readline
 
-data = input().rstrip()
+data = input().strip() # 개행 지우기 
+data = input().rstrip() 
 print(data)
 
 
+# String -> list 로 한 글자씩 넣는 법
+arr = list(str)
+
+#String 띄워쓰기 기준으로 나눠서 list 에 저장
+arr = str.split(' ')
+
+# 기본 정렬 라이브러리 사용시 tuple 이용해서 key 값으로 정렬 하는 방법
+arr = sorted(arr, key = lambda x: x[0])
+
+# 기본 정렬 라이브러리 사용시 tuple 이용해서 key 값으로 내림차순 정렬 하는 방법 -- key = lambda x : -x[0] 마이너스 붙이기
+arr = sorted(arr, key = lambda x : -x[1])
+
+# 여러개 기준으로 정렬해야하는 경우
+arr = sorted(arr, key = lambda x : (x[0], x[1], x[2]))
+
+# x, y 에 대해 동시 정렬할때 그냥 sorted 만 사용해도 자동 정렬 가능
+arr = sorted(arr) 
+
+# 정렬하는 방법
+arr.sort()
+sorted(arr)
+
+# 내림차순 정렬 하는 방법
+arr = sorted(arr, reverse = True)
+
+# 계수 정렬 알고리즘 ( 수의 범위가 1 ~ 10000 )
+# 배열의 인덱스를 특정한 데이터의 값으로 여기는 정렬 방법
+# 데이터가 등장한 횟수를 셈
+
+# 정수 -> String
+str(integer)
+
+# String 만들때 + 이용해서 만들기
+string += str(integers[i]) + operators[i]
+
+# eval()
+eval(string.replace(" ", ""))
+
+# 재귀함수 순환 횟수 제한 풀기 
+sys.setrecursionlimit(100000)
+
+# 문자를 하나씩 확인하며 숫자일때만 더하기
+# isdigit() == '0' <= x and x <= '9'
+for x in str :
+    if x.isdigit() :
+        result += int(x)
+
+# 하나의 문자를 해당 문자에 해당하는 유니코드 정수를 반환하는 함수
+ord('a') # => 97
+
+# 하나의 정수를 해당 정수에 해당하는 유니코드 문자를 반화하는 함수
+chr(97) # => 'a'
+
+# 원형 큐 => 회전하는 배열을 사용하기 위해서 반드시 "deque" 사용
+q = deque()
+q.rotate(1) # => 증가하는 방향으로 1만큼 회전
+q.rotate(-1) # => 감소하는 방향으로 1 만큼 회전
