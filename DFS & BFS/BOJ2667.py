9ed74317b54b13c5e90763368e8622dad451c0dc
oowhmilk <rlagusdn3046@naver.com>
2024-09-02 11:19:56 +0900

    [BOJ] 단지번호붙이기 / 실버 1 / 성공

--- DFS & BFS/BOJ2667.py
+++ DFS & BFS/BOJ2667.py
@@ -49,19 +49,74 @@
 #     print(x)
 
 
+# import sys
+# input = sys.stdin.readline
+
+# from collections import deque
+
+# def bfs(x, y) :
+#     global count 
+#     q = deque([(x, y)])
+
+#     dir = {(-1, 0), (0, -1), (1, 0), (0, 1)}
+#     while q :
+#         x, y = q.pop()
+
+#         for dx, dy in dir :
+#             nx = x + dx
+#             ny = y + dy
+
+#             if nx < 0 or n <= nx or ny < 0 or n <= ny :
+#                 continue
+#             if arr[nx][ny] == 1 and not visited[nx][ny] :
+#                 visited[nx][ny] = True
+#                 q.append((nx, ny))
+#                 count += 1
+
+
+# n = int(input())
+# arr = [[0] * (n) for _ in range(n)]
+# visited = [[False] * (n) for _ in range(n)]
+
+# for i in range(n) :
+#     str = input().strip()
+#     change = list(str)
+#     for j in range(n) :
+#         if change[j] == '0' :
+#             arr[i][j] = 0
+#         else : 
+#             arr[i][j] = 1
+
+# total = 0
+# result = []
+# for i in range(n) :
+#     for j in range(n) :
+#         if arr[i][j] == 1 and not visited[i][j] :
+#             count = 1
+#             visited[i][j] = True
+#             bfs(i, j)
+#             total += 1
+#             result.append(count)
+
+# print(total)
+# for x in sorted(result) :
+#     print(x)
+
+
+from collections import deque
 import sys
 input = sys.stdin.readline
 
-from collections import deque
-
 def bfs(x, y) :
-    global count 
-    q = deque([(x, y)])
+    count = 1
+    q = deque()
+    q.append((x, y))
+    visited[x][y] = True
 
-    dir = {(-1, 0), (0, -1), (1, 0), (0, 1)}
+    dir = {(-1, 0), (1, 0), (0, 1), (0, -1)}
+
     while q :
-        x, y = q.pop()
-
+        x, y = q.popleft()
         for dx, dy in dir :
             nx = x + dx
             ny = y + dy
@@ -73,31 +128,33 @@ def bfs(x, y) :
                 q.append((nx, ny))
                 count += 1
 
+    return count
+
 
 n = int(input())
-arr = [[0] * (n) for _ in range(n)]
-visited = [[False] * (n) for _ in range(n)]
 
+arr = [[0] * n for _ in range(n)]
 for i in range(n) :
-    str = input().strip()
-    change = list(str)
-    for j in range(n) :
-        if change[j] == '0' :
+    str = list(input().strip())
+
+    for j in range(len(str)) :
+        if str[j] == '0' :
             arr[i][j] = 0
-        else : 
+        else :
             arr[i][j] = 1
 
+visited = [[False] * n for _ in range(n)]
 total = 0
 result = []
+
 for i in range(n) :
-    for j in range(n) :
-        if arr[i][j] == 1 and not visited[i][j] :
-            count = 1
-            visited[i][j] = True
-            bfs(i, j)
-            total += 1
+    for j in range(n) : 
+         if arr[i][j] == 1 and not visited[i][j] :
+            total += 1 
+            count = bfs(i, j)
             result.append(count)
 
+result.sort()
 print(total)
-for x in sorted(result) :
+for x in result :
     print(x)
