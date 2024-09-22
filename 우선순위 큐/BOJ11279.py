5f7a3d95884f0c4ddb7f11ba15c16e683e215f5d
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-09-22 18:54:51 +0900

    [BOJ] 최대 힙 / 실버 2 / 성공

--- 우선순위 큐/BOJ11279.py
+++ 우선순위 큐/BOJ11279.py
@@ -1,21 +1,40 @@
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
 
+# import heapq
+
+# n = int(input())
+
+# abs_heap = []
+
+# for _ in range(n) :
+#     x = int(input())
+
+#     if x == 0 :
+#         if len(abs_heap) == 0 :
+#             print(0)
+#         else :
+#             count = heapq.heappop(abs_heap)
+#             print(-1 * count)
+#     else :
+#         heapq.heappush(abs_heap, -1 * x)
+
+
+
+import sys
 import heapq
+input = sys.stdin.readline
 
 n = int(input())
 
-abs_heap = []
-
+heap = []
 for _ in range(n) :
     x = int(input())
 
     if x == 0 :
-        if len(abs_heap) == 0 :
+        if len(heap) == 0 :
             print(0)
         else :
-            count = heapq.heappop(abs_heap)
-            print(-1 * count)
+            print(abs(heapq.heappop(heap)))
     else :
-        heapq.heappush(abs_heap, -1 * x)
-
+        heapq.heappush(heap, -1 * x)
