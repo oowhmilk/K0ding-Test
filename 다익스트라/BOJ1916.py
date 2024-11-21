446df631151831467ed0907675747af9bf17bef3
oowhmilk <rlagusdn3046@naver.com>
2024-11-21 16:51:25 +0900

    [BOJ] 최소비용 구하기 / 골드 5 / 성공

--- 다익스트라/BOJ1916.py
+++ 다익스트라/BOJ1916.py
@@ -73,9 +73,45 @@
 # print(distance[end])
 
 
+# import sys
+# input = sys.stdin.readline
+# import heapq
+
+# def dijkstra(start) :
+#     heap = []
+#     heapq.heappush(heap, (0, start))
+#     distance[start] = 0
+
+#     while heap :
+#         dist, now = heapq.heappop(heap)
+
+#         if distance[now] < dist :
+#             continue
+
+#         for i in arr[now] :
+#             cost = dist + i[1]
+
+#             if cost < distance[i[0]] :
+#                 distance[i[0]] = cost
+#                 heapq.heappush(heap, (cost, i[0]))
+
+
+# n = int(input())
+# m = int(input())
+
+# arr = [[] for _ in range(n + 1)]
+# for _ in range(m) :
+#     a, b, c = map(int, input().split(' '))
+#     arr[a].append((b, c))
+
+# start, end = map(int, input().split(' '))
+# distance = [int(1e9)] * (n + 1)
+# dijkstra(start)
+# print(distance[end])
+
+import heapq
 import sys
 input = sys.stdin.readline
-import heapq
 
 def dijkstra(start) :
     heap = []
@@ -87,14 +123,13 @@ def dijkstra(start) :
 
         if distance[now] < dist :
             continue
-
+        
         for i in arr[now] :
             cost = dist + i[1]
-
+        
             if cost < distance[i[0]] :
                 distance[i[0]] = cost
                 heapq.heappush(heap, (cost, i[0]))
-
 
 n = int(input())
 m = int(input())
@@ -105,6 +140,7 @@ for _ in range(m) :
     arr[a].append((b, c))
 
 start, end = map(int, input().split(' '))
+
 distance = [int(1e9)] * (n + 1)
 dijkstra(start)
 print(distance[end])
