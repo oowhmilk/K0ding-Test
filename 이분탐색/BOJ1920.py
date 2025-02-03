5c690ebbbd5cf7dbba7a27ea1ba74ef316832bf5
oowhmilk <rlagusdn3046@naver.com>
2025-02-03 12:55:55 +0900

    [BOJ] 수 찾기 / 실버 4 / 성공

--- 이분탐색/BOJ1920.py
+++ 이분탐색/BOJ1920.py
@@ -1,13 +1,31 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-a = list(map(int, input().split(' ')))
-m = int(input())
-b = list(map(int, input().split(' ')))
+def binary_search(x, left, right) :
 
-for value in b :
-    if value not in a :
-        print("0")
-    else :
+    global arr
+
+    while left <= right :
+        mid = int((left + right) // 2)
+
+        if arr[mid] == x :
+            return True
+        elif arr[mid] < x : 
+            left = mid + 1
+        else :
+            right = mid - 1
+
+    return False
+
+n = int(input())
+arr = list(map(int, input().split(' ')))
+arr.sort()
+
+m = int(input())
+check = list(map(int, input().split(' ')))
+
+for x in check :
+    if binary_search(x, 0, len(arr) - 1) :
         print("1")
+    else :
+        print("0")
