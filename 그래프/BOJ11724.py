7baae884aecfba0de30adf7c55b16e7a3ad30afb
oowhmilk <rlagusdn3046@naver.com>
2025-02-03 13:07:40 +0900

    [BOJ] 연결 요소의 개수 / 실버 2 / 성공

--- 그래프/BOJ11724.py
+++ 그래프/BOJ11724.py
@@ -1,68 +1,33 @@
-# import sys
-# input = sys.stdin.readline
-
-# def find_parent(parent, x) :
-#     if parent[x] != x :
-#         parent[x] = find_parent(parent, parent[x])
-#     return parent[x]
-
-# def union_parent(parent, x, y) :
-#     a = find_parent(parent, x)
-#     b = find_parent(parent, y)
-
-#     if a < b :
-#         parent[b] = a
-#     else :
-#         parent[a] = b
-
-# n, m = map(int, input().split())
-# parent = [0] * (n + 1)
-
-# for i in range(1, n + 1) :
-#     parent[i] = i
-
-# for i in range(m) :
-#     x, y = map(int, input().split())
-#     union_parent(parent, x , y)
-
-# counter = set()
-# for i in range(1, n + 1):
-#     counter.add(find_parent(parent, i))
-
-# print(len(counter))
-
-
+from collections import deque
 import sys
 input = sys.stdin.readline
 
-from collections import deque
+def bfs(x) :
+    q = deque()
+    q.append(x)
+    visited[x] = True
 
-def bfs(start) :
-    q = deque([start])
-    
     while q :
-        v = q.popleft()
+        x = q.popleft()
 
-        for x in arr[v] :
-            if not visited[x] :
-                visited[x] = True
-                q.append(x)
-
+        for next in arr[x] :
+            if not visited[next] :
+                visited[next] = True
+                q.append(next)
 
 n, m = map(int, input().split(' '))
 arr = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
 
 for _ in range(m) :
-    x, y = map(int, input().split(' '))
-    arr[x].append(y)
-    arr[y].append(x)
+    a, b = map(int, input().split(' '))
+    arr[a].append(b)
+    arr[b].append(a)
 
-visited = [False] * (n + 1)
-cnt = 0
-for i in range(1, n + 1) :
-    if not visited[i] :
-        visited[i] = True
-        bfs(i)
-        cnt += 1
+count = 0
+for x in range(1, n + 1) :
+    if not visited[x] :
+        count += 1
+        bfs(x)
 
-print(cnt)
+print(count)
