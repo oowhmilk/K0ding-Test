a26ed45d099fd2e2d92d17871c6a146364c4803b
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2025-02-03 22:27:09 +0900

    [BOJ] 도시 분할 계획 / 골드 4 / 성공

--- 최소신장트리/BOJ16398.py
+++ 최소신장트리/BOJ16398.py
@@ -1,55 +1,18 @@
-# import sys
-# input = sys.stdin.readline
-
-# def get_parent(parent, x) :
-#     if parent[x] == x :
-#         return x
-#     return get_parent(parent, parent[x])
-
-# def union_parent(parent, x, y) :
-#     a = get_parent(parent, x)
-#     b = get_parent(parent, y)
-
-#     if a < b :
-#         parent[b] = a
-#     else :
-#         parent[a] = b
-
-# def find_parent(parent, x, y) : 
-#     a = get_parent(parent, x)
-#     b = get_parent(parent, y)
-
-#     if a == b :
-#         return True
-#     else :
-#         return False
-
-# n = int(input())
-
-# arr = [list(map(int, input().split())) for _ in range(n)]  # 각 행성간의 플로우 관리 비용
-# parent = [i for i in range(n)]  # 부모 노드 정보
-
-# edges = []  
-# for i in range(1, n):
-#     for j in range(i):
-#         edges.append((i, j, arr[i][j]))
-# edges.sort(key = lambda x : x[2])  
-
-# result = 0
-# for a, b, c in edges :
-#     if not find_parent(parent, a, b) :
-#         union_parent(parent, a, b)
-#         result += c
-
-# print(result)
-
-
 import sys
 input = sys.stdin.readline
 
+def find_parent(parent, a, b) :
+    x = get_parent(parent, a)
+    y = get_parent(parent, b)
+
+    if x == y :
+        return True
+    else :
+        return False
+    
 def get_parent(parent, x) :
     if parent[x] == x :
-        return x
+        return x 
     return get_parent(parent, parent[x])
 
 def union_parent(parent, a, b) :
@@ -59,32 +22,24 @@ def union_parent(parent, a, b) :
     if x < y :
         parent[y] = x
     else :
-        parent[x] = y
+        parent[x] = y 
 
-def find_parent(parent, a, b) : 
-    x = get_parent(parent, a)
-    y = get_parent(parent, b)
+n, m = map(int, input().split(' '))
 
-    if x == y :
-        return True
-    else : 
-        return False
-
-n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]  # 각 행성간의 플로우 관리 비용
-
-edges = []  
-for i in range(1, n):
-    for j in range(i):
-        edges.append((i, j, arr[i][j]))
-edges.sort(key = lambda x : x[2])  
+arr = []
+for _ in range(m) :
+    a, b, c = map(int, input().split(' '))
+    arr.append((a, b, c))
 
 parent = [i for i in range(n + 1)]
+arr.sort(key = lambda x : x[2])
 
 result = 0
-for a, b, c in edges :
-    if not find_parent(parent, a, b) : 
+max_value = 0
+for a, b, c in arr :
+    if not find_parent(parent, a, b) :
         union_parent(parent, a, b)
-        result += c
+        result += c 
+        max_value = max(max_value, c)
 
-print(result)
+print(result - max_value)
