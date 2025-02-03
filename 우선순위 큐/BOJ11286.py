6fe3ef73ba44c6581d9ea07175c5673452fdf839
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2025-02-03 21:41:57 +0900

    [BOJ] 절댓값 힙 / 실버 1 / 성공

--- 우선순위 큐/BOJ11286.py
+++ 우선순위 큐/BOJ11286.py
@@ -1,39 +1,19 @@
-# import sys
-# input = sys.stdin.readline
-
-# import heapq
-# heap = []
-# n = int(input())
-
-# for _ in range(n) :
-#     num = int(input())
-
-#     if num == 0:
-#         if len(heap) == 0 :
-#             print(0)
-#         else :
-#             absolute, original = heapq.heappop(heap)
-#             print(original) 
-#     else :
-#         heapq.heappush(heap, (abs(num), num))
-
+import heapq
 import sys
 input = sys.stdin.readline
 
-import heapq
-
-n = int(input())
 heap = []
 
-for _ in range(n) :
+for _ in range(int(input())) :
     x = int(input())
-
+    
     if x == 0 :
+        
         if len(heap) == 0 :
             print(0)
-        else : 
-            absolute, real = heapq.heappop(heap)
-            print(real)
+        else :
+            abs_x, real_x = heapq.heappop(heap)
+            print(real_x)
 
     else :
         heapq.heappush(heap, (abs(x), x))
