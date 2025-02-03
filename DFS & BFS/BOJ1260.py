9bb0af56932131f8395d33b7dde3b514a43a3636
oowhmilk <rlagusdn3046@naver.com>
2025-02-03 11:19:58 +0900

    [BOJ] DFS와 BFS / 실버 2 / 성공

--- DFS & BFS/BOJ1260.py
+++ DFS & BFS/BOJ1260.py
@@ -1,151 +1,29 @@
-# from collections import deque
-
-# def dfs(v) :
-#     print(v, end = ' ')
-#     visited[v] = True
-#     for e in adj[v] :
-#         if not (visited[e]) :
-#             dfs(e)
-
-# def bfs(v) :
-#     q = deque([v])
-#     while q:
-#         v = q.popleft()
-#         if not (visited[v]) :
-#             visited[v] = True
-#             print(v, end = ' ')
-#             for e in adj[v] :
-#                 if not visited[e]:
-#                     q.append(e)
-
-# n, m, v = map(int, input().split(' '))
-# adj = [[] for _ in range(n + 1)]
-
-# for _ in range(m) :
-#     x, y = map(int, input().split(' '))
-#     adj[x].append(y)
-#     adj[y].append(x)
-
-# for e in adj :
-#     e.sort()
-
-# visited = [False] * (n + 1)
-# dfs(v)
-# print()
-# visited = [False] * (n + 1)
-# bfs(v)
-
-
-# import sys
-# from collections import deque
-
-# def dfs(v) :
-#     print(v, end = ' ')
-#     visited[v] = True
-#     for e in adj[v] :
-#         if not (visited[e]) :
-#             dfs(e)
-
-# def bfs(v) :
-#     q = deque([v])
-#     while q :
-#         v = q.popleft()
-#         if not (visited[v]) :
-#             visited[v] = True
-#             print(v, end = ' ')
-#             for e in adj[v] :
-#                 if not visited[e] :
-#                     q.append(e)
-
-# n, m, v = map(int, input().split(' '))
-# adj = [[] for _ in range(n + 1)]
-
-# for _ in range(m) :
-#     x, y = map(int, input().split(' '))
-#     adj[x].append(y)
-#     adj[y].append(x)
-
-# for e in adj :
-#     e.sort()
-
-# visited = [False] * (n + 1)
-# dfs(v)
-# print()
-# visited = [False] * (n + 1)
-# bfs(v)
-
-
-# import sys
-# input = sys.stdin.readline
-
-# from collections import deque
-
-# def dfs(v) :
-#     print(v, end = ' ')
-#     visited[v] = True
-#     for e in arr[v] :
-#         if not visited[e] :
-#             dfs(e)
-
-# def bfs(v) :
-#     q = deque()
-#     q.append(v) 
-
-#     while q :
-#         v = q.popleft()
-#         if not (visited[v]) :
-#             visited[v] = True
-#             print(v, end = ' ')
-
-#             for e in arr[v] :
-#                 if not visited[e] :
-#                     q.append(e)
-
-
-# n, m, v = map(int, input().split(' '))
-# arr = [[] for _ in range(n + 1)]
-
-# for _ in range(m) :
-#     x, y = map(int, input().split(' '))
-#     arr[x].append(y)
-#     arr[y].append(x)
-
-# for e in arr :
-#     e.sort()
-
-# visited = [False] * (n + 1)
-# dfs(v)
-# print()
-# visited = [False] * (n + 1)
-# bfs(v)
-
-
+from collections import deque
 import sys
 input = sys.stdin.readline
-from collections import deque
 
-def dfs(x) :
-    visited[x] = True
-    print(x, end = ' ')
+def dfs(start) :
+    visited[start] = True
+    print(start, end = ' ')
 
-    for next in arr[x] :
-        if not visited[next] :
-            dfs(next)
+    for x in arr[start] :
+        if not visited[x] :
+            dfs(x)
 
-def bfs(x) :
-    visited[x] = True
+
+def bfs(start) :
     q = deque()
-    q.append(x)
+    q.append(start)
+    visited[start] = True
 
     while q :
-        x = q.popleft()
-        print(x, end = ' ')
+        v = q.popleft()
+        print(v, end = ' ')
         
-        for next in arr[x] :
-            if not visited[next] :
-                visited[next] = True
-                q.append(next)
-
+        for x in arr[v] :
+            if not visited[x] :
+                q.append(x)
+                visited[x] = True
 
 n, m, v = map(int, input().split(' '))
 arr = [[] for _ in range(n + 1)]
@@ -160,6 +38,8 @@ for e in arr :
 
 visited = [False] * (n + 1)
 dfs(v)
+
 print()
+
 visited = [False] * (n + 1)
 bfs(v)
