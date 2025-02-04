4027987c3f0b63c0476931ab7926bd9dc0696862
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2025-02-05 01:10:25 +0900

    [BOJ] 최소비용 구하기 2 / 골드 3 / 성공

--- 다익스트라/BOJ11779.py
+++ 다익스트라/BOJ11779.py
@@ -1,21 +1,18 @@
+import heapq
 import sys
 input = sys.stdin.readline
 
-import heapq
-
-def dijkstra(start) :
+def dijkstra(start, end) :
+    heap = []
     heapq.heappush(heap, (0, start))
     distance[start] = 0
 
     while heap :
         dist, now = heapq.heappop(heap)
-
         if now == end :
             break
-        
-        if distance[now] < dist :
+        if dist < distance[now] :
             continue
-
         for i in arr[now] :
             cost = dist + i[1]
             if cost < distance[i[0]] :
@@ -24,20 +21,17 @@ def dijkstra(start) :
                 heapq.heappush(heap, (cost, i[0]))
 
 n = int(input())
-m = int(input())
 
 arr = [[] for _ in range(n + 1)]
-for _ in range(m) :
+for _ in range(int(input())) :
     a, b, c = map(int, input().split(' '))
     arr[a].append((b, c))
 
 start, end = map(int, input().split(' '))
 
 distance = [int(1e9)] * (n + 1)
-heap = []
 prev = [0] * (n + 1)
-
-dijkstra(start)
+dijkstra(start, end)
 
 print(distance[end])
 
