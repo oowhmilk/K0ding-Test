18210919b648d79c0f9bc54ff02778e223e0e2c6
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2025-02-04 23:41:43 +0900

    [BOJ] 숫자 카드 2 / 실버 4 / 성공

--- 이분탐색/BOJ10816.py
+++ 이분탐색/BOJ10816.py
@@ -1,21 +1,40 @@
 import sys
 input = sys.stdin.readline
 
+def binary_search(x) :
+    l = 0
+    r = len(check) - 1
+
+    while l <= r :
+        mid = int((l + r) // 2)
+
+        if check[mid] == x :
+            return True
+        
+        if check[mid] < x :
+            l = mid + 1
+        elif x < check[mid] :
+            r = mid - 1
+
+    return False
+
+
 n = int(input())
 arr = list(map(int, input().split(' ')))
+arr.sort()
 
 dic = {}
 for x in arr :
-    if x not in dic :
-        dic[x] = 1
-    else :
+    if x in dic :
         dic[x] += 1
+    else :
+        dic[x] = 1
+
+check = sorted(dic)
 
 m = int(input())
-arr = list(map(int, input().split(' ')))
-
-for x in arr :
-    if x not in dic :
-        print(0, end = ' ')
-    else :
+for x in list(map(int, input().split(' '))) :
+    if binary_search(x) :
         print(dic[x], end = ' ')
+    else :
+        print(0, end = ' ')
