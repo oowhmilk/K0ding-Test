1d3b6f0acbbd358a758519400414e27e7088b26a
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2025-02-05 21:53:06 +0900

    [BOJ] 1, 2, 3 더하기 / 실버 3 / 성공

--- DP/BOJ9095.py
+++ DP/BOJ9095.py
@@ -3,13 +3,16 @@ input = sys.stdin.readline
 
 for _ in range(int(input())) :
     n = int(input())
-    arr = [1, 2, 4]
+    dp = [0] * (n + 1)
 
-    if 3 < n :
-        for i in range(3, n) :
-            value = 0
-            for j in range(1, 4) :
-                value += arr[i - j]
-            arr.append(value)
-
-    print(arr[n - 1])
+    for i in range(1, n + 1) :
+        if i == 1 :
+            dp[i] = 1
+        elif i == 2 :
+            dp[i] = 2
+        elif i == 3 :
+            dp[i] = 4
+        else :
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1]
+    
+    print(dp[n])
