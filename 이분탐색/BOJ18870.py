cbd03e5c542d57f514e7b58320b2eb77f10d02ca
oowhmilk <rlagusdn3046@naver.com>
2025-02-10 18:10:33 +0900

    [BOJ] 좌표 압축 / 실버 2 / 성공

--- 이분탐색/BOJ18870.py
+++ 이분탐색/BOJ18870.py
@@ -1,14 +1,26 @@
 import sys
 input = sys.stdin.readline
 
+def binary_search(x) :
+    l = 0
+    r = len(sorted_arr)
+
+    while l <= r :
+        mid = (int(l + r) // 2)
+
+        if sorted_arr[mid] == x :
+            return mid
+        
+        elif sorted_arr[mid] < x :
+            l = mid + 1
+        else :
+            r = mid - 1
+
+
 n = int(input())
 arr = list(map(int, input().split(' ')))
 sorted_arr = set(arr)
 sorted_arr = sorted(sorted_arr)
 
-data = {}
-for i in range(len(sorted_arr)) :
-    data[sorted_arr[i]]= i
-
 for x in arr :
-    print(data[x], end = ' ')
+    print(binary_search(x), end = ' ')
