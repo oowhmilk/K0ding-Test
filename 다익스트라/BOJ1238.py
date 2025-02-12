4ab7c217fff97f7f6a050af6a6f07fcdc791b3b3
oowhmilk <rlagusdn3046@naver.com>
2025-02-12 15:55:21 +0900

    [BOJ] 파티 / 골드 3 / 성공

--- 다익스트라/BOJ1238.py
+++ 다익스트라/BOJ1238.py
@@ -1,26 +1,24 @@
+import heapq
 import sys
 input = sys.stdin.readline
 
-import heapq
-
-def dijkstra(start) :
-    distance[start] = 0
-
+def djikstra(start) :
     heap = []
+    distance[start] = 0
     heapq.heappush(heap, (0, start))
 
     while heap :
         dist, now = heapq.heappop(heap)
+
         if distance[now] < dist :
             continue
-
         
         for i in arr[now] :
             cost = dist + i[1]
+
             if cost < distance[i[0]] :
                 distance[i[0]] = cost
                 heapq.heappush(heap, (cost, i[0]))
-
 
 n, m, x = map(int, input().split(' '))
 
@@ -29,23 +27,17 @@ for _ in range(m) :
     a, b, c = map(int, input().split(' '))
     arr[a].append((b, c))
 
-go = [0]
-back = []
-
-for i in range(n) :
+go = [0] * (n + 1)
+for i in range(1, n + 1) :
     distance = [int(1e9)] * (n + 1)
-    dijkstra(i + 1)
+    djikstra(i)
+    go[i] = distance[x]
 
-    if i + 1 == x :
-        back = distance
-        go.append(0)
-    else :
-        go.append(distance[x])
+distance = [int(1e9)] * (n + 1)
+djikstra(x)
 
-max_size = -1
-for i in range(n + 1) :
-    result = go[i] + back[i]
-    if result < 1000000000 :
-        max_size = max(result, max_size)
+max_size = 0
+for i in range(1, n + 1) :
+    max_size = max(max_size, go[i] + distance[i])
 
 print(max_size)
