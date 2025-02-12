6be68937fe1ed0106414682cee3dcd47b1b59733
oowhmilk <rlagusdn3046@naver.com>
2025-02-12 17:47:35 +0900

    [BOJ] 상근이의 여행 / 실버 4 / 성공

--- 최소신장트리/BOJ9372.py
+++ 최소신장트리/BOJ9372.py
@@ -3,6 +3,8 @@ input = sys.stdin.readline
 
 for _ in range(int(input())) :
     n, m = map(int, input().split(' '))
+
     for _ in range(m) :
-        x, y = map(int, input().split(' '))
+        a, b = map(int, input().split(' '))
+    
     print(n - 1)
