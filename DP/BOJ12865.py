7ee22425ff4eab61c320adb8d6501a123c27132f
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2025-02-24 23:21:23 +0900

    [BOJ] 평범한 배낭 / 골드 5 / 성공

--- DP/BOJ12865.py
+++ DP/BOJ12865.py
@@ -1,31 +1,17 @@
-# 배낭 문제
-# 배낭을 가능한 배낭 무게 * 경우의 수 로 초기화
-# 경우 마다 들어오는 배낭의 무게 까지는 이전의 값 그대로 가져오기 -> 왜냐하면 테이블이 특정 무게에 대한 최대 가치 값을 가지고 있기 때문에
-# 배낭 무게 보다 무거운 경우 : 덧셈 이용해서 최댓 값인 배낭을 저장
-
-n, k = map(int, input().split())
-dp = [[0] * (k + 1) for _ in range(n + 1)]
-
-for i in range(1, n + 1):
-    weight, value = map(int, input().split())
-    for j in range(1, k + 1):
-        if j < weight:
-            dp[i][j] = dp[i - 1][j]
-        else:
-            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight] + value)
-
-print(dp[n][k])
-
+import sys
+input = sys.stdin.readline
 
 n, k = map(int, input().split(' '))
+
 dp = [[0] * (k + 1) for _ in range(n + 1)]
 
 for i in range(1, n + 1) :
-    w, v = map(int, input().split(' '))
-    for j in range(1, k + 1) :
+    weight, value = map(int, input().split(' '))
+
+    for j in range(k + 1) :
         if j < weight :
             dp[i][j] = dp[i - 1][j]
         else :
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - w] + v)
+            dp[i][j] = max(dp[i - 1][j - weight] + value, dp[i - 1][j])
 
-print(dp[n][k])    
+print(dp[n][k])
