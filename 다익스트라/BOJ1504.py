e408dd4fd09c84f571d1a5fa8bec7bb99ef9eebd
oowhmilk <rlagusdn3046@naver.com>
2025-03-04 10:15:01 +0900

    [BOJ] 특정한 최단 경로 / 골드 4 / 성공

--- 다익스트라/BOJ1504.py
+++ 다익스트라/BOJ1504.py
@@ -1,24 +1,23 @@
-import sys
 import heapq
+import sys
 input = sys.stdin.readline
 
 def dijkstra(start) :
     heap = []
-    distance[start] = 0
     heapq.heappush(heap, (0, start))
+    distance[start] = 0
 
     while heap :
         dist, now = heapq.heappop(heap)
+
         if distance[now] < dist :
             continue
 
-        for i in arr[now] : 
+        for i in arr[now] :
             cost = dist + i[1]
-
             if cost < distance[i[0]] :
-                distance[i[0]] = cost
+                distance[i[0]] = cost 
                 heapq.heappush(heap, (cost, i[0]))
-
 
 n, e = map(int, input().split(' '))
 
@@ -28,23 +27,32 @@ for _ in range(e) :
     arr[a].append((b, c))
     arr[b].append((a, c))
 
-a, b = map(int, input().split(' '))
+fir, sec = map(int, input().split(' '))
 
 distance = [int(1e9)] * (n + 1)
+# 1 -> fir
+# 1 -> sec
 dijkstra(1)
-d_1_to_a = distance[a]
-d_1_to_b = distance[b]
+start_to_fir = distance[fir]
+start_to_sec = distance[sec]
 
 distance = [int(1e9)] * (n + 1)
-dijkstra(a)
-d_a_to_b = distance[b]
-d_a_to_n = distance[n]
+# fir -> sec
+# fir -> n
+dijkstra(fir)
+fir_to_sec = distance[sec]
+fir_to_n = distance[n]
 
 distance = [int(1e9)] * (n + 1)
-dijkstra(b)
-d_b_to_a = distance[a]
-d_b_to_n = distance[n]
+# sec -> fir
+# sec -> n
+dijkstra(sec)
+sec_to_fir = distance[fir]
+sec_to_n = distance[n]
 
+result = min(start_to_fir + fir_to_sec + sec_to_n, start_to_sec + sec_to_fir + fir_to_n)
 
-result = min(d_1_to_a + d_a_to_b + d_b_to_n, d_1_to_b + d_b_to_a + d_a_to_n)
-print(result)
+if int(1e9) <= result :
+    print(-1)
+else :
+    print(result)
