d4bc613931e8aacc339e350545c6332c30c3767f
oowhmilk <rlagusdn3046@naver.com>
2025-03-14 16:15:08 +0900

    [BOJ] 타일 채우기 / 골드 4 / 성공

---
+++ DP/BOJ2133.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0] * 31
+dp[0] = 1
+dp[2] = 3
+
+for i in range(4, n + 1, 2) :
+    dp[i] = dp[i - 2] * 3
+
+    for j in range(i - 4, -1, -2) :
+        dp[i] += dp[j] * 2
+
+print(dp[n])
