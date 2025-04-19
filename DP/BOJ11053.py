d12d3e6591e55384aad5f53cfa2dbb2f9e4eb258
oowhmilk <rlagusdn3046@naver.com>
2025-04-19 22:57:46 +0900

    [BOJ] 가장 긴 증가하는 부분 수열 / 실버  2 / 성공

--- DP/BOJ11053.py
+++ DP/BOJ11053.py
@@ -4,21 +4,36 @@
 # 확인하는 인덱스 기준 이전 인덱스 값들을 확인하면서 확인하는 인덱스 값이 더 클 경우에 이전 인덱스 값마다 + 1 을 하고 비교하기
 # 결국 DP 문제이기 때문에 이전 값들을 사용할 생각을 하면서 값을 구해야한다. 
 
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# arr = list(map(int, input().split(' ')))
+
+# dp = [1] * n
+
+# for i in range(n) :
+#     for j in range(i) :
+#         if arr[j] < arr[i] :
+#             dp[i] = max(dp[i], dp[j] + 1)
+
+# print(max(dp))
+
+
+# n = int(input())
+# arr = list(map(int, input().split(' ')))
+
+# dp = [1] * n
+
+# for i in range(n) :
+#     for j in range(n) :
+#         if arr[i] < arr[j] :
+#             dp[i] = max(dp[i], dp[j] + 1)
+
+# print(max(dp))
+
 import sys
 input = sys.stdin.readline
-
-n = int(input())
-arr = list(map(int, input().split(' ')))
-
-dp = [1] * n
-
-for i in range(n) :
-    for j in range(i) :
-        if arr[j] < arr[i] :
-            dp[i] = max(dp[i], dp[j] + 1)
-
-print(max(dp))
-
 
 n = int(input())
 arr = list(map(int, input().split(' ')))
