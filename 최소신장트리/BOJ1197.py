0202364fbad66fe01d732ea50e43247c34e1f058
oowhmilk <rlagusdn3046@naver.com>
2025-04-19 23:32:01 +0900

    [BOJ] 최소 스패닝 트리 / 골드 4 / 성공

--- 최소신장트리/BOJ1197.py
+++ 최소신장트리/BOJ1197.py
@@ -1,33 +1,78 @@
+# import sys
+# input = sys.stdin.readline
+# sys.setrecursionlimit(100000)
+
+# def find_parent(x, y) :
+#     a = get_parent(x)
+#     b = get_parent(y)
+
+#     if a == b :
+#         return True
+#     else :
+#         return False
+
+# def union_parent(x, y) :
+#     a = get_parent(x)
+#     b = get_parent(y)
+
+#     if a < b :
+#         parent[b] = a
+#     else :
+#         parent[a] = b
+
+# def get_parent(x) :
+#     if parent[x] != x :
+#         parent[x] = get_parent(parent[x])
+#         return parent[x]
+#     return x
+
+# v, e = map(int, input().split(' '))
+
+# arr = []
+# for _ in range(e) :
+#     a, b, c = map(int, input().split(' '))
+#     arr.append((a, b, c))
+
+# arr = sorted(arr, key = lambda x : x[2])
+
+# parent = [i for i in range(v + 1)]
+# answer = 0
+
+# for a, b, c in arr :
+#     if not find_parent(a, b) :
+#         union_parent(a, b)
+#         answer += c
+
+# print(answer)
+
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(100000)
 
-def find_parent(x, y) :
-    a = get_parent(x)
-    b = get_parent(y)
+def find_parent(a, b) :
+    x = get_parent(parent, a)
+    y = get_parent(parent, b)
 
-    if a == b :
+    if x == y :
         return True
     else :
         return False
+    
+def union_parent(a, b) :
+    x = get_parent(parent, a)
+    y = get_parent(parent, b)
 
-def union_parent(x, y) :
-    a = get_parent(x)
-    b = get_parent(y)
-
-    if a < b :
-        parent[b] = a
+    if x < y :
+        parent[y] = x
     else :
-        parent[a] = b
+        parent[x] = y
 
-def get_parent(x) :
+def get_parent(parent, x) :
     if parent[x] != x :
-        parent[x] = get_parent(parent[x])
-        return parent[x]
+        return get_parent(parent, parent[x])
     return x
 
 v, e = map(int, input().split(' '))
-
 arr = []
 for _ in range(e) :
     a, b, c = map(int, input().split(' '))
@@ -36,11 +81,10 @@ for _ in range(e) :
 arr = sorted(arr, key = lambda x : x[2])
 
 parent = [i for i in range(v + 1)]
-answer = 0
-
+cost = 0
 for a, b, c in arr :
     if not find_parent(a, b) :
         union_parent(a, b)
-        answer += c
+        cost += c
 
-print(answer)
+print(cost)
