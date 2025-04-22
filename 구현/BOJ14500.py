dca4ae03172e6671f7e4d21887b1c4655e7e13f2
oowhmilk <rlagusdn3046@naver.com>
2025-04-22 23:01:18 +0900

    [BOJ] 테트로미노 / 골드 4 / 성공

--- 구현/BOJ14500.py
+++ 구현/BOJ14500.py
@@ -79,3 +79,41 @@ for i in range(n) :
         dfs(1, arr[i][j], [(i, j)])
 
 print(ans)
+
+def dfs(cnt, temp, lst) :
+    global ans
+
+    if cnt == 4 :
+        ans = max(ans, temp)
+        return
+    
+    dir = {(-1, 0), (0, -1), (0, 1), (1, 0)}
+    for cx, cy in lst :
+        
+        for dx, dy in dir :
+            nx = cx + dx
+            ny = cy + dy
+
+            if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == 0 :
+                visited[nx][ny] = 1
+                dfs(cnt + 1, temp + arr[nx][ny], lst + [(nx, ny)])
+                visited[nx][ny] = 0
+
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split(' '))
+
+arr = []
+for _ in range(n) :
+    arr.append(list(map(int, input().split(' '))))
+
+ans = 0
+visited = [[0] * m for _ in range(n)]
+for i in range(n) :
+    for j in range(m) :
+        visited[i][j] = 1
+        dfs(1, arr[i][j], [(i, j)])
+
+print(ans)
