2fc496ee26ae1166b9eadcc3edd2e1a88dd083e5
oowhmilk <rlagusdn3046@naver.com>
2025-04-23 00:11:00 +0900

    [BOJ] 상어 초등학교 / 골드 5 / 성공

--- 구현/BOJ21608.py
+++ 구현/BOJ21608.py
@@ -1,109 +1,88 @@
 import sys
 input = sys.stdin.readline
 
+def find_like(x, y, like) :
+    dir = {(-1, 0), (1, 0), (0, -1), (0, 1)}
+
+    count = 0
+    for dx, dy in dir :
+        nx = x + dx
+        ny = y + dy
+
+        if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] in like :
+            count += 1
+
+    return count
+
+def find_zero(x, y) :
+    dir = {(-1, 0), (1, 0), (0, -1), (0, 1)}
+
+    count = 0
+    for dx, dy in dir :
+        nx = x + dx
+        ny = y + dy
+
+        if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] == 0 :
+            count += 1
+
+    return count
+
 n = int(input())
+
 arr = [[0] * n for _ in range(n)]
 
-input_arr = []
+save = [0] * (n ** 2 + 1)
 for _ in range(n ** 2) :
-    input_arr.append(list(map(int, input().split(' '))))
+    now, one, two, three, four = map(int, input().split(' '))
+    like = [one, two, three, four]
 
-for data in input_arr :
+    save[now] = like
 
-    student = data[0]
-
-    # 비어있는 칸 중에 좋아하는 학생 몇명있는지 확인
-    score = [[0] * n for _ in range(n)]
-    
-    for x in range(n) :
-        for y in range(n) :
-            count = 0
-            for k in range(1, len(data)) :
-
-                check = data[k]
-                dir = {(-1, 0), (0, 1), (0, -1), (1, 0)}
-                for dx, dy in dir :
-                    nx = x + dx
-                    ny = y + dy
-
-                    if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] == check :
-                        count += 1
-
-            score[x][y] = count
-
-    # 비어있는 칸 중에 좋아하는 학생 수의 큰 값을 찾기
-    empty = 0
-    input_x = -1
-    input_y = -1
-
-    max_score = -1
+    like_count = -1
+    like_count_arr = []
     for i in range(n) :
         for j in range(n) :
-            if max_score < score[i][j] and arr[i][j] == 0 :
-                max_score = score[i][j]
-                input_x = i
-                input_y = j
+            if arr[i][j] == 0 :
+                count = find_like(i, j, like)
 
-    
-    for x in range(n) :
-        for y in range(n) :
+                if like_count < count : 
+                    like_count = count
+                    like_count_arr = []
+                    like_count_arr.append((i, j))
+                elif like_count == count :
+                    like_count_arr.append((i, j))
 
-            count = 0
-            if max_score == score[x][y] and arr[x][y] == 0 :
-                dir = {(-1, 0), (1, 0), (0, -1), (0, 1)}
-                for dx, dy in dir :
-                    nx = x + dx
-                    ny = y + dy 
+    zero_count = -1
+    zero_count_arr = []
+    for x, y in like_count_arr :
+        count = find_zero(x, y)
 
-                    if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] == 0 :
-                        count += 1
+        if zero_count < count :
+            zero_count = count
+            zero_count_arr = []
+            zero_count_arr.append((x, y))
+        elif zero_count == count :
+            zero_count_arr.append((x, y))
 
-            if count > empty :
-                empty = count
-                input_x = x
-                input_y = y
+    zero_count_arr.sort()
 
-    arr[input_x][input_y] = student
+    select_x, select_y = zero_count_arr[0]
+    arr[select_x][select_y] = now
 
+answer = 0
+for i in range(n) :
+    for j in range(n) :
+        count = find_like(i, j, save[arr[i][j]])
 
+        if count == 0 :
+            answer += 0
+        elif count == 1 :
+            answer += 1
+        elif count == 2 :
+            answer += 10
+        elif count == 3 :
+            answer += 100
+        elif count == 4 :
+            answer += 1000
 
-ans = 0
-for data in input_arr :
-    input = data[0]
-
-    for i in range(n) :
-        for j in range(n) :
-
-            x = i
-            y = j
-            count = 0
-
-            if input == arr[i][j] :
-                
-                for k in range(1, len(data)) : # 넣을려는 값들의 좋아하는 값들이 주변에 있는지 확인하기
-                
-                    check = data[k] # 좋아하는 값
-                    directions = {(-1, 0), (1, 0), (0, -1), (0, 1)}
-                    for dx, dy in directions :
-                        nx = x + dx
-                        ny = y + dy
-
-                        if nx < 0 or n <= nx or ny < 0 or n <= ny :
-                            continue
-
-                        if check == arr[nx][ny] :
-                            count += 1
-
-                
-            if count == 0 : 
-                ans += 0
-            elif count == 1:
-                ans += 1
-            elif count == 2:
-                ans += 10
-            elif count == 3:
-                ans += 100
-            elif count == 4:
-                ans += 1000
-
-print(ans)
+print(answer)
