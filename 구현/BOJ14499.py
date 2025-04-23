6c6d9f5ca5438824a683e8583ec3d3e760d2cc20
oowhmilk <rlagusdn3046@naver.com>
2025-04-24 00:02:20 +0900

    [BOJ] 주사위 굴리기 / 골드 4 / 성공

--- 구현/BOJ14499.py
+++ 구현/BOJ14499.py
@@ -7,36 +7,39 @@ arr = []
 for _ in range(n) :
     arr.append(list(map(int, input().split(' '))))
 
-command = list(map(int, input().split(' ')))
+command_arr = list(map(int, input().split(' ')))
+
 dice = [0] * 6
 
-dir = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+dir = [(0, 0), (0, 1), (0, -1), (-1, 0), (1, 0)]
+for command in command_arr :
 
-for com in command :
-    dx, dy = dir[com - 1]
+    dx, dy = dir[command]
+
     nx = x + dx
     ny = y + dy
 
-    if nx < 0 or n <= nx or ny < 0 or m <= ny :
-        continue
+    if 0 <= nx < n and 0 <= ny < m :
+        east, west, south, north, up, down = dice[0], dice[1], dice[2], dice[3], dice[4], dice[5]
 
-    east, west, south, north, up, down = dice[0], dice[1], dice[2], dice[3], dice[4], dice[5]
+        if command == 1 :
+            dice[0], dice[1], dice[4], dice[5] = down, up, east, west
+        elif command == 2 :
+            dice[0], dice[1], dice[4], dice[5] = up, down, west, east
+        elif command == 3 :
+            dice[2], dice[3], dice[4], dice[5] = up, down, north, south
+        elif command == 4 :
+            dice[2], dice[3], dice[4], dice[5] = down, up, south, north
 
-    if com == 1 :
-        dice[0], dice[1], dice[4], dice[5] = down, up, east, west
-    elif com == 2 :
-        dice[0], dice[1], dice[4], dice[5] = up, down, west, east
-    elif com == 3 :
-        dice[2], dice[3], dice[4], dice[5] = up, down, north, south
-    elif com == 4 :
-        dice[2], dice[3], dice[4], dice[5] = down, up, south, north
+        if arr[nx][ny] == 0 :
+            arr[nx][ny] = dice[5]
+        else :
+            dice[5] = arr[nx][ny] 
+            arr[nx][ny] = 0
 
-    if arr[nx][ny] == 0 :
-        arr[nx][ny] = dice[5]
+        x = nx
+        y = ny
+        print(dice[4])
+
     else :
-        dice[5] = arr[nx][ny] 
-        arr[nx][ny] = 0
-
-    x = nx
-    y = ny
-    print(dice[4])
+        continue
