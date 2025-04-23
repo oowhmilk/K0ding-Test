9c3bc797f768f1fc7c6b3e7ef7b4e38fb2b9a243
oowhmilk <rlagusdn3046@naver.com>
2025-04-23 17:49:36 +0900

    [BOJ] 마법사 상어와 비바라기 / 골드 5 / 성공

--- 구현/BOJ21610.py
+++ 구현/BOJ21610.py
@@ -1,58 +1,66 @@
 import sys
 input = sys.stdin.readline
 
-def rain(d, s) :
-    positions = []
-    for cloud in clouds :
-        x, y = cloud
-        dx, dy = dir[d - 1]
-        x = (x + dx * s) % n
-        y = (y + dy * s) % n
-        arr[x][y] += 1
-        positions.append((x, y))
+def rain_move(rain, rain_dir, count) :
+    moved_rain = []
 
-    return positions
+    dx, dy = dir[rain_dir]
+    for x, y in rain :
+        nx = (x + (dx * count) + n) % n
+        ny = (y + (dy * count) + n) % n
+
+        moved_rain.append((nx, ny))
+
+    return moved_rain
+
+def rain_magic(x, y) :
+    count = 0
+
+    cross_dir = {(-1, -1), (-1, 1), (1, 1), (1, -1)}
+    for dx, dy in cross_dir :
+        nx = x + dx
+        ny = y + dy
+
+        if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] != 0 :
+            count += 1
+
+    return count
+
 
 n, m = map(int, input().split(' '))
-clouds = [(n - 1, 0), (n - 1, 1), (n - 2, 0), (n - 2, 1)]
 
-dir = [(0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1)]
 arr = []
-
 for _ in range(n) :
     arr.append(list(map(int, input().split(' '))))
 
+rain = [(n - 1, 0), (n - 1, 1),(n - 2, 0), (n - 2, 1)]
+dir = [(0, 0), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1)]
 for _ in range(m) :
     d, s = map(int, input().split(' '))
-    positions = rain(d, s)
 
-    for position in positions :
-        x, y = position
-        cnt = 0
-        
-        for i in range(1, 8, 2) :
-            dx, dy = dir[i]
-            nx = x + dx
-            ny = y + dy
+    # 비구름 이동
+    moved_rain = rain_move(rain, d, s)
 
-            if nx < 0 or n <= nx or ny < 0 or n <= ny :
-                continue
-            if arr[nx][ny] >= 1 :
-                cnt += 1 
+    # 물의 양 증가
+    for x, y in moved_rain :
+        arr[x][y] += 1
 
-        arr[x][y] += cnt
-    
-    positions = set(positions)
-    clouds = []
+    # moved_rain 에서 각 정점마다 대각선 값 확인
+    for x, y in moved_rain :
+        count = rain_magic(x, y)
+        arr[x][y] += count
 
+    # 구름 만들기 
+    rain = []
     for i in range(n) :
         for j in range(n) :
-            if arr[i][j] >= 2 and (i, j) not in positions :
-                clouds.append((i, j))
+            if 2 <= arr[i][j] and (i, j) not in moved_rain :
+                rain.append((i, j))
                 arr[i][j] -= 2
 
 answer = 0
-for row in arr  :
-    answer += sum(row)
+for i in range(n) :
+    for j in range(n) :
+        answer += arr[i][j]
 
 print(answer)
