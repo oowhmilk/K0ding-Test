8700c0e30d9a2bb253b40e061d152a70114be461
oowhmilk <rlagusdn3046@naver.com>
2025-04-25 00:09:40 +0900

    [BOJ] 최단경로 / 골드 4 / 성공

--- 다익스트라/BOJ1753.py
+++ 다익스트라/BOJ1753.py
@@ -1,3 +1,42 @@
+# import heapq
+# import sys
+# input = sys.stdin.readline
+
+# def dijkstra(start) :
+#     heap = []
+#     heapq.heappush(heap, (0, start))
+#     distance[start] = 0
+
+#     while heap :
+#         dist, now = heapq.heappop(heap)
+
+#         if distance[now] < dist :
+#             continue
+
+#         for i in arr[now] :
+#             cost = dist + i[1]
+
+#             if cost < distance[i[0]] :
+#                 distance[i[0]] = cost
+#                 heapq.heappush(heap, (cost, i[0]))
+
+# v, e = map(int, input().split(' '))
+# k = int(input())
+
+# arr = [[] for _ in range(v + 1)]
+# for _ in range(e) :
+#     a, b, c = map(int, input().split(' '))
+#     arr[a].append((b, c))
+
+# distance = [int(1e9)] * (v + 1)
+# dijkstra(k)
+
+# for i in range(1, v + 1) :
+#     if distance[i] == int(1e9) :
+#         print("INF")
+#     else :
+#         print(distance[i])
+
 import heapq
 import sys
 input = sys.stdin.readline
@@ -21,18 +60,19 @@ def dijkstra(start) :
                 heapq.heappush(heap, (cost, i[0]))
 
 v, e = map(int, input().split(' '))
-k = int(input())
+start = int(input())
 
 arr = [[] for _ in range(v + 1)]
 for _ in range(e) :
     a, b, c = map(int, input().split(' '))
     arr[a].append((b, c))
 
-distance = [int(1e9)] * (v + 1)
-dijkstra(k)
+distance = [(int(1e9))] * (v + 1)
+
+dijkstra(start)
 
 for i in range(1, v + 1) :
     if distance[i] == int(1e9) :
         print("INF")
-    else :
+    else : 
         print(distance[i])
