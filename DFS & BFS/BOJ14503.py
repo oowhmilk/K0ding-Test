dd54e0498e0918e15cdebbd0d178c08389c03d5e
oowhmilk <rlagusdn3046@naver.com>
2025-05-01 23:04:31 +0900

    [BOJ] 로봇 청소기 / 골드 5 / 성공

--- DFS & BFS/BOJ14503.py
+++ DFS & BFS/BOJ14503.py
@@ -1,60 +1,54 @@
-from collections import deque 
+from collections import deque
 import sys
 input = sys.stdin.readline
 
-def bfs(x, y, now) :
-    global result
-
-    dir = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+def bfs(start_x, start_y, count) :
+    global d
     q = deque()
-    q.append((x, y))
-    visited[x][y] = True
+    q.append((start_x, start_y, count))
+    visited[start_x][start_y] = True
 
+    dir = [(-1, 0), (0 ,1), (1, 0), (0, -1)]
     while q :
-        x, y = q.popleft()
+        x, y, count = q.popleft()
 
-        clean = False
-        change = now
-        for i in range(4) :
-            change = (change + 3) % 4
-            dx, dy = dir[change]
+        change = False
+        for dx, dy in dir :
+            nx = x + dx
+            ny = y + dy 
 
+            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and arr[nx][ny] != 1 :
+                change = True
+
+        if change :
+            d = ((d - 1)+ 4) % 4
+            dx, dy = dir[d]
             nx = x + dx
             ny = y + dy
 
-            if 0 <= nx < n and 0 <= ny < m :
-                if not visited[nx][ny] and arr[nx][ny] == 0 :
-                    visited[nx][ny] = True
-                    q.append((nx, ny))
-                    clean = True
-                    now = change
-                    result += 1
-                    break
+            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and arr[nx][ny] != 1 :
+                q.append((nx, ny, count + 1))
+                visited[nx][ny] = True
+            else :
+                q.append((x, y, count))
 
-        if not clean :
-            dx, dy = dir[(now + 2) % 4]
-
+        else :
+            dx, dy = dir[(d + 2) % 4]
             nx = x + dx
             ny = y + dy
 
-            if 0 <= nx < n and 0 <= ny < m :
-                if arr[nx][ny] == 1 :
-                    print(result)
-                    return
-                else :
-                    visited[nx][ny] = True
-                    q.append((nx, ny))
-
-
+            if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] != 1 :
+                q.append((nx, ny, count))
+            else :
+                return count
 
 n, m = map(int, input().split(' '))
-r, c, d = map(int, input().split(' '))
+robot_x, robot_y, d = map(int, input().split(' '))
 
 arr = []
 for _ in range(n) :
     arr.append(list(map(int, input().split(' '))))
 
 visited = [[False] * m for _ in range(n)]
-
-result = 1
-bfs(r, c, d)
+result = bfs(robot_x, robot_y, 1)
+print(result)
